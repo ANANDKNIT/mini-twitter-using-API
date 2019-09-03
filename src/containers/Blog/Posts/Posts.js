a9d43c7725ee0c8e82c1041268a7810d6f06032a
ANANDKNIT <anand.sharma0510@gmail.com>
2019-09-03 19:24:18 +0530

    removed the Link and added programatically navigation

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "../../../axios";
 import Post from "../../../components/Post/Post";
 import "./Posts.css";
-import { Link } from "react-router-dom";
+
 class Posts extends Component {
   state = {
     posts: [],
@@ -33,7 +33,8 @@ class Posts extends Component {
 
   postSelectedHandler = id => {
     console.log(id);
-    this.setState({ selectedPostId: id });
+    // this.setState({ selectedPostId: id });
+    this.props.history.push({ pathName: "/" + id });
   };
 
   render() {
@@ -41,13 +42,12 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link to={"/" + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
       });
     } else {
