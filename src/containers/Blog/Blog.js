bc31167fe08a8fb470d6172a1acf9ee6e48b20d9
ANANDKNIT <anand.sharma0510@gmail.com>
2019-09-03 15:48:09 +0530

    added Link to aviod refreshing the page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "./Posts/Posts"
-import {Route } from "react-router-dom"
+import {Route,Link } from "react-router-dom"
 import NewPost from "./NewPost/NewPost";
 class Blog extends Component {
 
@@ -9,8 +9,12 @@ class Blog extends Component {
     return (
       <div>
         <header className="Blog"><ul>
-          <li><a href="/"> Home</a></li>
-          <li><a href="/new-post">NewPost</a></li>
+          <li><Link to="/"> Home</Link></li>
+          <li><Link to={{
+            pathname:"new-post",
+          hash:"#submit",
+          search:"?anand?awsad"
+          }}>NewPost</Link></li>
           </ul></header>
           <Route exact path="/" component={Posts} />
           <Route path="/new-post" component={NewPost}/>
