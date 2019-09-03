59921f1a25e8a7b8f1a8bb52362810129a5ab1df
ANANDKNIT <anand.sharma0510@gmail.com>
2019-09-03 15:25:36 +0530

    restructure the project and added sample routing in home

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,73 +1,19 @@
 import React, { Component } from "react";
-
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
-import axios from "../../axios";
-
+// import Posts from "./Posts/Posts"
+import {Route } from "react-router-dom"
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-  componentDidMount() {
-    //     const ApiData= axios.get("https://jsonplaceholder.typicode.com/posts"); 
-    //the above code will not give the correct result because it will execute the method immediately so to avoid this use promises
-    axios
-      .get("/posts")
-      .then(response => {
-        const posts = response.data.slice(0, 6);
-        const updatedData = posts.map(post => {
-          return {
-            ...post,
-            author: "Anand"
-          };
-        });
-        console.log(updatedData)
-        this.setState({ posts: updatedData });
-      })
-      .catch(error => {
-        console.log(error);
-        this.setState({ error: true });
-      });
-  }
-  postSelectedHandler = id => {
-    console.log(id);
-    this.setState({ selectedPostId: id });
-  };
 
   render() {
-    let posts;
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    } else {
-      posts = <p>Something Went Wrong</p>;
-    }
-
     return (
       <div>
         <header className="Blog"><ul>
           <li><a href="/"> Home</a></li>
           <li><a href="/new-post">NewPost</a></li>
           </ul></header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+          <Route exact path="/" render={()=><h1>Home</h1>} />
+          <Route path="/" render={()=><h1>Home2</h1>} />
+
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post"
+import "./Posts.css"
+
+class Posts extends Component {
+
+    state = {
+        posts:[],
+        error:null
+    }
+
+  componentDidMount() {
+    //     const ApiData= axios.get("https://jsonplaceholder.typicode.com/posts");
+    //the above code will not give the correct result because it will execute the method immediately so to avoid this use promises
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 6);
+        const updatedData = posts.map(post => {
+          return {
+            ...post,
+            author: "Anand"
+          };
+        });
+        console.log(updatedData);
+        this.setState({ posts: updatedData });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = id => {
+    console.log(id);
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    } else {
+      posts = <p>Something Went Wrong</p>;
+    }
+
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
